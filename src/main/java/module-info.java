40220df567409559ebb9c5e1e7092fd6cc5339d1
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 13:17:53 +0300

    drop ide api deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,27 @@
  */
 module consulo.restclient
 {
-	requires consulo.ide.api;
+	requires consulo.application.api;
+	requires consulo.base.icon.library;
+	requires consulo.code.editor.api;
+	requires consulo.component.api;
+	requires consulo.datacontext.api;
+	requires consulo.disposer.api;
+	requires consulo.document.api;
+	requires consulo.language.api;
+	requires consulo.language.editor.api;
+	requires consulo.language.editor.ui.api;
+	requires consulo.localize.api;
+	requires consulo.logging.api;
+	requires consulo.project.api;
+	requires consulo.project.ui.api;
+	requires consulo.ui.api;
+	requires consulo.ui.ex.api;
+	requires consulo.ui.ex.awt.api;
+	requires consulo.util.collection;
+	requires consulo.util.jdom;
+	requires consulo.util.lang;
+	requires consulo.virtual.file.system.api;
 
 	requires org.apache.httpcomponents.core5.httpcore5;
 	requires org.apache.httpcomponents.core5.httpcore5.h2;
